5cd436707abc0ca7ed5d42248e90f049408cdca1
zabmehta26 <41826709+zabmehta26@users.noreply.github.com>
2020-04-29 19:02:55 +0530

    Add files via upload

--- movie_database/src/Controller/testing.php
+++ movie_database/src/Controller/testing.php
@@ -72,7 +72,6 @@ class testing extends ControllerBase{
         '#rows' => $items,
       ];
 
-  public function test() {
       return $this->render(
         array(
           '#theme' => 'temp',
@@ -82,6 +81,83 @@ class testing extends ControllerBase{
       );
   }
 }
+
+
+  public function actorMovie (NodeInterface $node) {
+
+    // Get Name of the actor whose id is passed.
+    $title = $node->title->value;
+    // Get nid of the current node.
+    $nid = $node->nid->value;
+    // Query for fetching out all movies in which actor has worked.
+    $query = \Drupal::entityQuery('node')
+        ->condition('status', 1)
+        ->condition('type','movie')
+        ->condition('field_actor_role.entity:paragraph.field_actor.target_id',$nid);
+        $nids = $query->execute();
+    // check if query returned something or not.
+    if (empty($nids)){
+      $data = array("#markup" => "No Results Found");
+      }
+    else{
+      $movie_nodes = entity_load_multiple('node', $nids);
+      $items = array();
+      foreach ($movie_nodes as $node) {
+        $ratings = $node->field_rating->getValue ();
+        $rating = $ratings[0]['rating'];
+        $rating = $rating/20;
+        $rating = $rating."/"."5";
+        $node_title = $node->title->value;
+        $node_id = $node->nid->value;
+        $node_des = $node->field_s->value;
+        $node_image_fid = $node->field_poster->target_id;
+        if ( !is_null($node_image_fid) ) {
+          $image_entity = \Drupal\file\Entity\File::load($node_image_fid);
+          $node_poster = $image_entity->url();
+        }
+        else {
+               $image_entity_url = "/sites/default/files/default_images/obama.jpg";
+        }
+
+        $target_id = array();
+        $target_id = $node ->field_actor_role->getValue();
+        $actors = array();
+        $j = 0;
+        $coactors = array ();
+        foreach ($target_id as $value) {
+          $paragraph = Paragraph::load($value['target_id']);
+          $actor_id = $paragraph->field_actor->target_id;
+
+          $actor = Node::load($actor_id);
+          $actors[$j]['name'] = $actor->title->value;
+          $actors[$j]['nid'] = $actor->nid->value;
+          if ($actor_id != $nid ) {
+            $coactors[$j]['name'] = $actor->title->value;
+            $coactors[$j]['nid'] = $actor->nid->value;
+          }
+          $j++;
+        }
+        // kint ($coactors);
+
+        $items[] = [
+          'name' => $node_title,
+          'nid' => $node_id,
+          'des' => $node_des,
+          'poster' => $node_poster,
+          'actors' =>$actors,
+          'ratings' =>$rating,
+          'costars' => $coactors
+        ];
+      }
+      $mTitle = 'List of '.$title.' movies';
+      return array (
+        '#theme' => 'actormovie_list',
+        '#items' => $items,
+        '#title' => $mTitle,
+      );
+    }
+  }
+
 
 
 }
